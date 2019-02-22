fbffff679f2634193f11c3d462c9f25731ac336b
Anant Prasad <anantprsd5@gmail.com>
2019-02-22 21:54:43 +0530

    Added headlines functionality

--- app/src/main/java/com/socialcops/newsapp/Adapter/NewsAdapter.java
+++ app/src/main/java/com/socialcops/newsapp/Adapter/NewsAdapter.java
@@ -1,11 +1,15 @@
 package com.socialcops.newsapp.Adapter;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.socialcops.newsapp.Model.Articles;
 import com.socialcops.newsapp.R;
 
@@ -13,6 +17,7 @@ import java.util.List;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.CustomViewHolder> {
     private List<Articles> articles;
+    private Context context;
 
     public NewsAdapter(List<Articles> articles) {
         this.articles = articles;
@@ -24,13 +29,22 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.CustomViewHold
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.articles_list, parent, false);
 
+        context = parent.getContext();
         return new CustomViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(CustomViewHolder holder, int position) {
         Articles article = articles.get(position);
-        holder.articleName.setText(article.getTitle());
+        holder.articleNameView.setText(article.getTitle());
+        holder.articleSourceView.setText(article.getSource().getName());
+        holder.articleDateView.setText(article.getPublishedAt());
+
+        Glide
+                .with(context)
+                .load(article.getUrlToImage())
+                .centerCrop()
+                .into(holder.articleImageView);
     }
 
     @Override
@@ -40,11 +54,17 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.CustomViewHold
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView articleName;
+        public TextView articleNameView;
+        public TextView articleSourceView;
+        public TextView articleDateView;
+        public ImageView articleImageView;
 
         public CustomViewHolder(View view) {
             super(view);
-            articleName = view.findViewById(R.id.articleName);
+            articleNameView = view.findViewById(R.id.news_title);
+            articleDateView = view.findViewById(R.id.news_date);
+            articleSourceView = view.findViewById(R.id.news_source);
+            articleImageView = view.findViewById(R.id.news_image);
         }
     }
 }
